d1cfae6014857df3b665c8997ba83bb1e56dc040
artur_aloyan <artur_aloyan@edu.aua.am>
2018-09-11 01:50:32 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,11 @@
+		const x = 2;
+		const m = 3;
+		const s = "Armen" ;
+		var z = x+m;
+
+		if (z === s.length) {
+			document.write("good job");
+			}
+		else {
+   		   document.write("Sorry");
+			}
